b48419a87f98bcf9c2977d92278b470423c2011a
Mustafa Ali <mustafaramakda61@gmail.com>
2026-04-13 16:17:19 +0300

    feat: update CORS origin to production URL for app and socket server

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -13,7 +13,7 @@ app.use(cookieParser());
 app.use(morgan("dev"));
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://perplexity-beryl.vercel.app/",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
   }),
